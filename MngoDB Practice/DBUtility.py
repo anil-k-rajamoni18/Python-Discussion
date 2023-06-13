4a079cc5952d2b5f4983f7abb9e88b55ee3369b0
fenil1998 <fenil2206@gmail.commit>
2023-06-13 10:54:32 -0400

    I am having issues in update method.

---
+++ MngoDB Practice/DBUtility.py
@@ -0,0 +1,44 @@
+import pymongo
+
+class MongoDBConnection:
+    def __init__(self,dbName , collName , dburl , port , hostName , userName = None , passwd= None):
+        self.__dbName = dbName
+        self.__collName = collName
+        self.__port = port
+        self.__host = hostName
+        self.__user_name = userName
+        self.Password = passwd
+        self.__dbURL = dburl
+        self.mydb = None
+        self.mycoll = None
+
+    def create_connection(self):
+        try: 
+            client = pymongo.MongoClient(self.__dbURL)  #client connection
+           
+            database_names = client.list_database_names()
+            print(f"Available databases: {database_names}")
+
+            if self.__dbName in database_names:
+                self.mydb = client[self.__dbName]
+
+                if self.__collName in self.mydb.list_collection_names():
+                    self.mycoll = self.mydb[self.__collName]
+                    print(f'connection successfull : {self.__dbName} : {self.__collName}')
+                    return self.mycoll
+                else:
+                    raise Exception("collection not found{e}")
+            else:
+                raise Exception("Database not found{e}")
+
+        except Exception as e:
+            print(f'exception occured while making connection{e}')
+            
+dbobj = MongoDBConnection('studentData','studentCollection','mongodb://localhost', 27017,'localhost')
+
+print(dbobj)
+
+coll = dbobj.create_connection()
+
+print(coll)
+
---
+++ MngoDB Practice/UtilityOperations.py
@@ -0,0 +1,127 @@
+
+from DBUtility import MongoDBConnection
+import datetime
+collobj = MongoDBConnection('studentData','studentCollection','mongodb://localhost', 27017,'localhost').create_connection()
+
+print(collobj)
+
+#CRUD Operation
+
+def insert_todo(**kwargs):
+    if collobj is not None:
+        data=kwargs
+        data['createDate'] = datetime.datetime.now()
+        try:
+            response=collobj.insert_one(data)
+            print(f'inserted{response.acknowledged} and {response.inserted_id} ')
+        except Exception as e:
+            print(e)
+
+def find_todo(id=None,title=None,cardColor =None):
+    if collobj is not None:
+        try:
+            if id:
+                resultset = collobj.find({"id":id})
+                for data in resultset:
+                    print(data)
+            elif title:
+                resultset = collobj.find({"title":title})
+                for data in resultset:
+                    print(data)
+            else:
+                resultset = collobj.find({},{"_id":0})
+                for data in resultset:
+                    print(data)
+           
+        except Exception as e:
+            print(e)
+    else:
+        return None 
+            
+
+def delete_todo(id):
+    if collobj is not None:
+        try:
+            result = collobj._delete({"id": id})
+            if result.deleted_count > 0:
+                print("Delete successful.")
+            else:
+                print("No document found with the specified todo_id.")
+        except Exception as e:
+            print(e)
+
+
+
+def update_todo(todo_id,updated_data):
+    todo_id = int(input("Enter id: "))
+    updated_data = {"title": input(), "cardColor": input()} 
+
+    updated_document = update_todo(todo_id, updated_data)
+    if updated_document:
+        print("Updated Document:")
+        print(updated_document)
+    if collobj is not None:
+        try:
+           result = collobj.update_one({"id":todo_id},{'$set': updated_data})
+           if result.modified_count > 0:
+                    print("Update successful.")
+                    updated_document = collobj.find_one({"id": todo_id})
+                    return updated_document
+           else:
+                    print("No document found with the specified todo_id.")
+                    return None
+        except Exception as e:
+            print(e)
+            return None
+    else:
+        return None
+
+
+
+def main():
+    while True:
+        print("*"*30)
+        print("""   1.Insertion 
+                    2.Find
+                    3.Find on id
+                    4.Find on title
+                    5.Update on id
+                    6.Delete on id
+                    -1.Exit   """)
+        print("*"*30)
+        choice =  int(input("Enter the choice: "))
+        if choice == 1:
+            id = int(input("Enter id: "))
+            title = input("Enter Task Name: ")
+            cardColor = input("Enter task status : Black, Blue, green: ")
+            isCompleted = bool(input("Is Task completed? true/false: "))
+            isCompleted = True if isCompleted == "true" else False
+                
+           
+            insert_todo( id =id, title=title, cardColor = cardColor, isCompleted=isCompleted )
+        
+        elif choice == 2:
+            find_todo()
+        
+        elif choice == 3:
+            id = int(input("Enter id: "))
+            find_todo(id =id)
+
+        elif choice == 4:
+            title= input("Enter Task Name: ")
+            find_todo(title = title)
+        
+        elif choice == 5:
+            id = int(input("Enter id: "))
+            update_todo(id = id )
+            
+
+        elif choice == 6:
+            id = int(input("Enter id: "))
+            delete_todo(id=id)
+
+        elif choice == -1:
+            break
+
+if __name__ == '__main__':
+    main()
